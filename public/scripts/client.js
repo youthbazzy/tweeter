0022d0236b328f0392cf1ea93f96b85a416c389a
Bazil <king.bazil.khan@gmail.com>
2022-05-31 21:10:53 +0000

    complete functional reqs

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -4,7 +4,9 @@
  * Reminder: Use (and do all your DOM work in) jQuery's document ready function
  */
 
-const { render } = require("express/lib/response");
+// const { render } = require("express/lib/response");
+
+// const render = require("express/lib/response");
 
 
 const createTweetElement = function(tweetObj) {
@@ -15,11 +17,11 @@ const createTweetElement = function(tweetObj) {
   const $tweet = $(`<article class="tweet-container">
     <header class="tweet-header">
       <div>            
-        <!-- <img src=""> -->
+        <i class="fa-solid fa-flag"></i>
         <span>${tweetObj.user.name}</span>
       </div>
       <div>
-        <span>${tweetObj.user.handle}</span>
+        <span class="handle">${tweetObj.user.handle}</span>
       </div>
     </header>
     <div class="tweet-body">
@@ -38,6 +40,7 @@ const createTweetElement = function(tweetObj) {
 }
 
 const renderTweets = function(objArr) {
+  $('#tweets-container').empty()
   for (let obj in objArr) {
     let $tweet = createTweetElement(objArr[obj])
     $('#tweets-container').append($tweet);
@@ -45,10 +48,12 @@ const renderTweets = function(objArr) {
 }
 
 const loadTweets = function() {
-  $.ajax('/tweets', { method: 'GET' })
+  $.ajax('/tweets/', { method: 'GET' })
     .then(function (tweets) {
       renderTweets(tweets)
     })};
+  // $.get("/tweets")
+
 
 $(document).ready(function()  {  
   loadTweets();
@@ -58,12 +63,27 @@ $(document).ready(function()  {
     event.preventDefault();
     console.log($(this))
     
+    if($('#error-box')) {
+      $('.error').remove()
+    }
+
     let tweetData = $(this).serialize()
     let tweetInfo = tweetData.substring(5)
+    let errorPresent = false;
 
-    if(tweetInfo == '' || tweetInfo.length > 140 )  {
-      alert('invalid tweet, check length and make sure its typed')
-    } else  {
+    if(tweetInfo == '') {
+      errorPresent = true;
+      let $error = $("<h3 class='error'>please type tweet</h3>") 
+      $('#error-box').append($error);
+    }
+    if(tweetInfo.length > 140)  {
+      errorPresent = true;
+      let $error = $("<h3 class='error'>tweet too long</h3>") 
+      $('#error-box').append($error);
+    } 
+    
+    if(!(errorPresent))  {
+
       let newTweet = {
         "user": {
           "name": "Me",
@@ -76,10 +96,12 @@ $(document).ready(function()  {
         "created_at": 1653703458814
       }
 
-      $.post("/tweets", tweetData, function(data, status){
-        alert("Data: " + tweetData + "\nStatus: " + status);
+      $.post("/tweets/", tweetData, function(data, status){
+        $('#tweet-text').val('')
+        loadTweets();
       });
     }
-    loadTweets();
+    // renderTweets()
+    // loadTweets()
   });
 });
